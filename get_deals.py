8452838f61c5cb2de9e84cdd4fbbe2a5102f8495
Mahdi Kafshgar <113425634+mahdikf78@users.noreply.github.com>
2023-05-10 16:47:11 +0330

    v1.9

--- get_deals.py
+++ get_deals.py
@@ -6,7 +6,7 @@ import os
 from bs4 import BeautifulSoup
 
 os.system('color')
-print('\033[33mSearch in Playstation GOLDEN WEEK\033[0m')
+print('\033[33mSearch in Playstation Deals\033[0m')
 
 def sent_sms():
     api = '64547446757776434366792B364836556E766F2B3351555133512F7A664A68363331394B6A5A45626E38733D'
@@ -18,9 +18,8 @@ def sent_sms():
 #sent_sms()
 
 games = []
-#link = []
+number = 0
 def get_deals(url):
-    number = 0
     soup = BeautifulSoup(requests.get(url).content, 'html.parser')
     #This is the Main <li> tag that all products is in that
     for products in soup.find_all('li', class_='psw-l-w-1/2@mobile-s psw-l-w-1/2@mobile-l psw-l-w-1/6@tablet-l psw-l-w-1/4@tablet-s psw-l-w-1/6@laptop psw-l-w-1/8@desktop psw-l-w-1/8@max'):
@@ -30,6 +29,7 @@ def get_deals(url):
         #Find Names
         for names in products.find_all('a'):
             n = json.loads(names['data-telemetry-meta'])
+            global number
             number = number + 1
             print('\033[33m-',number,':',n['name']+'\033[0m')
             games.append(f"{n['name'].lower()}, {n['price']}")
@@ -57,64 +57,75 @@ def show_cart():
     file = open('cart.txt','r')
     text = file.read()
     print(text)
+
 def search_games():
     while True:
+        print('')
+        print('Enter c for show cart')
         res = input('Search Game by Name : ')
-        if res == '--c':
+        if res.lower() == 'c':
             try:
                 print('')
                 show_cart()
-                aa = input('Enter to Exit ,, Type 0 to Delete Cart : ')
-                if aa == 'c' :
+                aa = input('Hit Enter to Back ,, Type 0 to Delete Cart : ')
+                if aa == '0' :
                     send2trash.send2trash('cart.txt')
-                    print('Cart Deleted')
+                    print('')
+                    print('\033[33mCart Deleted\033[0m')
+                    print('')
             except:
-                print('cart not found')
+                print('\033[31mCart not Found\033[0m')
                 print('')
         elif res == '.':
             games.clear()
             search_pages()
             break
-        res1 = [item for item in games if res in item]
-        for i in res1:
-            print('\033[32m',res1.index(i),'-',i,'\033[0m')
-        
-        #create cart in txt file
-        def add_cart():
-            if len(res1) >= 1 :
-                while True:
-                    add = input('type index to add cart : ')
-                    if add == '.':
-                        break
-                    else:
-                        try:
-                            print(res1[int(add)])
-                            file = open('cart.txt','a')
-                            file.write('- '+res1[int(add)]+'\n')
-                            print('added to cart.txt')
-                            file.close()
-                        except:
-                            print('wrong index !')
-        add_cart()
+        else:
+            res1 = [item for item in games if res in item]
+            for i in res1:
+                print('\033[32m',res1.index(i)+1,'-',i,'\033[0m')
+            #create cart in txt file
+            def add_cart():
+                if len(res1) >= 1 :
+                    while True:
+                        add = input('type number and enter to add cart or / for search again: ')
+                        if add == '/':
+                            break
+                        else:
+                            try:
+                                print('')
+                                print(res1[int(add)-1])
+                                file = open('cart.txt','a')
+                                file.write('- '+res1[int(add)-1]+'\n')
+                                print('added to cart.txt')
+                                print('')
+                                file.close()
+                            except:
+                                print('wrong index !')
+                                print('')
+            add_cart()
 
 def search_pages():
     while True:
-        page = input('what page? : ')
-        try:
-            if page == '--c':
+        print('')
+        print('Enter c for show cart')
+        page = input('How many pages should be searched? : ')
+        try:    
+            if page.lower() == 'c':
                 try:
                     print('')
                     show_cart()
-                    aa = input('Enter to Exit ,, Type 0 to Delete Cart : ')
-                    if aa == 'c' :
+                    aa = input('Hit Enter to Back ,, Type 0 to Delete Cart : ')
+                    if aa == '0' :
                         send2trash.send2trash('cart.txt')
-                        print('Cart Deleted')
+                        print('')
+                        print('\033[33mSearch in Playstation Deals\033[0m')
+                        print('')
                 except:
-                    print('cart not found')
-                    print('')
+                    print('\033[31mCart not Found\033[0m')
             else:
                 page = int(page) + 1
-                url = input('paste url here : ')
+                url = input('paste the discount url here and Enter : ')
                 del_part = url.rfind('1')
                 for i in range(1,int(page)):
                     get_deals(url[:del_part]+str(i)+'?FULL_GAME=storeDisplayClassification')
